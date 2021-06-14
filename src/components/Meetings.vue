2d8947bd8ac21b5e48ebcef00635e340e081eff7
Victor Pirat <pirat@hey.com>
2021-06-14 15:58:48 +0200

    add meeting description when hovering a calendar day

--- src/components/Meetings.vue
+++ src/components/Meetings.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <div class="flex items-center justify-between">
-      <h1 class="text-3xl">Meetings</h1>
+    <div class="flex items-baseline justify-between">
+      <div class="text-3xl">
+        <h1>Meetings</h1>
+        <span class="text-lg text-gray-400">{{ currentDayDescription }}</span>
+      </div>
       <menu class="flex gap-2 text-blue-400">
         <button class="focus:outline-none" @click="reset()">Reset</button>
         <button class="focus:outline-none" @click="daily()">Daily</button>
@@ -13,15 +16,29 @@
       <div
         v-for="(_ , day) in 365"
         :key="day"
-        :class="['h-4 rounded-sm cursor-pointer hover:bg-blue-100', ['bg-gray-200', 'bg-green-400', 'bg-yellow-400', 'bg-red-400'][store.state.meetings[day].duration]]"></div>
+        :class="['h-4 rounded-sm cursor-pointer hover:bg-blue-100', ['bg-gray-200', 'bg-green-400', 'bg-yellow-400', 'bg-red-400'][store.state.meetings[day].duration]]"
+        @mouseover="currentDay = day"
+        @mouseout="currentDay = -1"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue'
 import { useStore } from '../store'
 
 const store = useStore()
+
+const currentDay = ref(-1)
+const currentDayDescription = computed(() => {
+  if (currentDay.value != -1) {
+    const date = new Intl.DateTimeFormat('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).format(new Date(new Date().getFullYear(), 0, currentDay.value + 1))
+    const meetingDuration = store.state.meetings[currentDay.value].duration
+    return meetingDuration !== 0 ? `$date | ${meetingDuration}h` : date
+  }
+  const meetingsDuration = store.state.meetings.reduce((total, meeting) => total + meeting.duration, 0)
+  return meetingsDuration === 0 ? 'No meetings' : `${meetingsDuration}h of meetings this year`
+})
 
 const reset = () => {}
 
